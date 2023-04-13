f95c2f6bd4843d6815b84a19b3ac92a2770d0825
sbkurubar <105355226+sbkurubar@users.noreply.github.com>
2023-04-13 14:47:23 +0530

    Update and rename streamlit_app.py to sbk1.py

--- sbk1.py
+++ sbk1.py
@@ -4,9 +4,10 @@ import math
 import pandas as pd
 import streamlit as st
 
+
 """
 # Welcome to Streamlit!
-
+st.write("Welcome to Streamlit!")
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
